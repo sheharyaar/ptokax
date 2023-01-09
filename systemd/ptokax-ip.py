2f8f7465db87198f7443ddefecc06e00733fbbb5
Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
2023-01-10 00:59:28 +0530

    :wip improved ptokax-dhcp python script
    Signed-off-by: Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>

--- systemd/ptokax-ip.py
+++ systemd/ptokax-ip.py
@@ -1,6 +1,16 @@
 import netifaces
 
 
+class InterfaceNotFoundException(Exception):
+    "Interface not found"
+    pass
+
+
+class IPNotFoundException(Exception):
+    "IP Address not assigned to interface"
+    pass
+
+
 def get_interface_adddress(itf_name):
     idx = -1
 
@@ -8,20 +18,24 @@ def get_interface_adddress(itf_name):
         if itf == itf_name:
             idx = netifaces.interfaces().index(itf)
 
+    # Check if interface exists
     if idx == -1:
-        raise Exception(f"No interface \"{itf_name}\" found")
+        raise InterfaceNotFoundException(f"Interface \"{itf_name}\" not found")
 
-    return netifaces.ifaddresses(itf_name)
+    addrs = netifaces.ifaddresses(itf_name)
+
+    # Check if ip address exists
+    if netifaces.AF_INET not in addrs.keys():
+        raise IPNotFoundException("IP Address not assigned to interface")
+
+    ip = addrs.get(netifaces.AF_INET)
+    return ip[0]["addr"]
 
 
 if __name__ == "__main__":
     try:
-        addrs = get_interface_adddress("eth0")
+        addr = get_interface_adddress("eth0")
     except Exception as e:
         print("Error in get_interface_address :", e)
     else:
-        ip = addrs.get(netifaces.AF_INET)
-        if ip == "None":
-            print("No ip address assigned")
-        else:
-            print("IP Address : ", ip[0]["addr"])
+        print("IP Address : ", addr)
