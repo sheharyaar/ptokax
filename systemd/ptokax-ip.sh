78f93e717ba1ff5cd706feab5c9db98830ee07ba
Arpit Bhardwaj <proffapt@pm.me>
2023-01-11 17:49:14 +0000

    sh: re-writing int better syntax

--- systemd/ptokax-ip.sh
+++ systemd/ptokax-ip.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-PYTHON=$(which python3)
-if [[ "x$PYTHON" == "x" ]]; then
-    # Install python raspberry pi
-    echo "Python3 not found. Installing ..."
-fi
-
-echo "Python found at : $PYTHON"
-
-PIP=$(which pip3)
-if [[ "x$PIP" == "x" ]]; then
-    # Install pip raspberry pi
-    echo "Pip not found. Installing ..."
-fi
-
-echo "Pip found at : $PIP"
-
+PYTHON=$(which python3 || echo "python3")
+PIP=$(which pip3 || echo "pip3")
 MODULE=$(pip list | grep -cw "netifaces")
+
+for package in "$PYTHON" "$PIP"; do
+    if [[ $package != *"/"* ]]; then
+        echo "$cmd package not found. Installing ..."
+    	# install package
+        # sudo apt install "$package" # will this not work.. idk what happens when system boots 
+   	fi
+done
+
 if [[ $MODULE -eq 0 ]]; then
     echo "Netifaces module not found. Installing ..."
     $PIP install netifaces
