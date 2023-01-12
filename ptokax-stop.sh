01d6e7098bd178a2ef7b020696aca9bad477c667
proffapt <proffapt@pm.me>
2023-01-12 17:21:16 +0530

    removing syntax warnings

--- ptokax-stop.sh
+++ ptokax-stop.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 ptokaxId=$(pgrep PtokaX)
-if [ "$ptokaxId" == "" ]; then
+if [ -z "$ptokaxId" ]; then
 	echo "PtokaX is not running currently"
 	exit
 fi
 
-echo "PID : {$ptokaxId}"
+echo "PID : ${ptokaxId}"
 
-sudo kill -SIGTERM $ptokaxId
+sudo kill -SIGTERM "$ptokaxId"
 
 echo "Successfully stopped PtokaX!"
-
-exit
