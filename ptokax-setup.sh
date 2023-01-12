e643082a5de922ac33b839dce26c4b994af5b131
proffapt <proffapt@pm.me>
2023-01-12 23:27:52 +0530

    most compact code, as much as I can

--- ptokax-setup.sh
+++ ptokax-setup.sh
@@ -31,30 +31,19 @@ for action in "start" "stop" "setup"; do
 done
 
 printf "\n\n[log] Installing Prerequisites\n\n"
-# Install curl to download source code
-sudo apt install -y curl
-
-# Install g++, zlib, tinyxml
-sudo apt install -y make g++ zlib1g-dev libtinyxml-dev
-
-# Install lua 5.2.2
-# Latest lua not installed as scripts are in lua version 5.2.2
-sudo apt install -y liblua5.2-dev
-
-# Install mysql - needed for scripts
-sudo apt install -y default-libmysqlclient-dev lua-sql-mysql
+# Curl for downloading source code
+# mysql - required for scripts
+# LUA 5.2.2 - Latest lua not installed as scripts are in lua version 5.2.2
+sudo apt install -y curl liblua5.2-dev make g++ zlib1g-dev libtinyxml-dev default-libmysqlclient-dev lua-sql-mysql
 
 printf "\n\n[log] Downloading and installing PtokaX\n\n"
 # Download PtokaX source code
 curl -L https://github.com/sheharyaar/ptokax/releases/download/latest/ptokax-0.5.2.2-src.tgz -o ~/ptokax-0.5.2.2-src.tgz
-
 # Extract the archive
 tar -xf ~/ptokax-0.5.2.2-src.tgz
-
 # Make the program
 cd PtokaX/ || (echo "cd to PtokaX failed" && exit)
 make -f makefile-mysql lua52
-
 # Setup privileges in order to allow ptokax to run on port 411 (default port)
 sudo apt install -y libcap2-bin
 sudo make install
