64e60b74a674f5e48630854b34842c9a3e8b149c
proffapt <proffapt@pm.me>
2023-01-12 17:19:39 +0530

    using setsid to detach the process

--- ptokax-config.sh
+++ ptokax-config.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 ~/PtokaX/PtokaX -m
-exit
--- ptokax-start.sh
+++ ptokax-start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 ptokaxId=$(pgrep PtokaX)
-if [ "$ptokaxId" != "" ]; then
+if [ -z "$ptokaxId" ]; then
 	echo "PtokaX is already running on PID : ${ptokaxId}"
 	exit
 fi
 
-sudo ~/PtokaX/PtokaX -d -c ~/PtokaX/ &
+setsid sudo ~/PtokaX/PtokaX -d -c ~/PtokaX/
 echo "Successfully started PtokaX server!"
-exit
